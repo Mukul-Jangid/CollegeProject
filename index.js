84257f764fcb3bd8db1bb85f70446d9489e5a7f1
Mukul Jangid <mukul805809@gmail.com>
2022-12-17 08:36:59 +0530

    v2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const CookieParser = require('cookie-parser');
 const express = require('express');
+const cors = require('cors')
 const Connection = require('./controllers/db');
 const app = express();
 const customerRoute = require('./routes/customerRoute');
@@ -8,7 +9,7 @@ const retailerRoute = require('./routes/retailerRoute');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(CookieParser());
-
+app.use(cors())
 app.use('/api/v1',customerRoute);
 app.use('/api/v1',retailerRoute);
 // app.use('/api/v1',alertRoute)
