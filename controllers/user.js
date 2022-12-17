74702d0ac3861f11b1fb6ef710431f9536cdc2a9
Mukul Jangid <mukul805809@gmail.com>
2022-12-17 08:57:19 +0530

    v2

--- controllers/user.js
+++ controllers/user.js
@@ -17,9 +17,7 @@ exports.getUserByPhone = async (req,res)=>{
         const user = await User.findOne({phone: req.body.phone});
         console.log(user);
     if(user){
-        return res.status(200).json({
-           user
-        })
+        return res.status(200).json(user)
     }
     return res.status(404).json({
         error:"User not found",
